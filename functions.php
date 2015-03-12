2b50990253ef0d31a514d6791dd40978ca40ac88
Mike Spencer <mspencer@meetup.com>
2015-03-12 16:27:44 -0400

    don't need to reference main.min.css anymore

--- functions.php
+++ functions.php
@@ -3,7 +3,6 @@
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
   wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-  wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/dist/css/main.min.css' );
 }
 
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_scripts' );
